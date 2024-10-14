a23cf64f49b474ba7a9be7f7d7f1a826f65292c4
Vanshaj Bhardwaj <73098377+justvanshaj@users.noreply.github.com>
2024-10-14 21:51:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ header {visibility: hidden;}
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # Load the image (replace 'image.jpg' with your file path)
-img = Image.open('Banner.jpg')
+img = Image.open('Banner.png')
 
 # Display the image
 st.image(img, caption='', width=100)
